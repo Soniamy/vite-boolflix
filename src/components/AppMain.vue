e21c072e5100e1629d718a6bd1951f50489e7806
Soniamy <marianna.marzullo@gmail.com>
2024-01-25 09:41:24 +0100

    Inizio Malistone 3: ho collegato l'http fissa datami nel progetto con l'API

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,6 +19,13 @@ export default {
     <div>
       <h2 class="py-3">Film</h2>
       <div v-for="(movie, index) in store.movies" :key="index">
+        <div class="front">
+          <img
+            :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+            :alt="movie.original_title"
+            :title="movie.original_title"
+          />
+        </div>
         <h2 class="py-1">{{ movie.title }}</h2>
         <h3 class="py-1">Titolo Originale: {{ movie.original_title }}</h3>
         <span>Lingua:</span>
@@ -32,6 +39,14 @@ export default {
       </div>
       <h2 class="py-3">Serie Tv</h2>
       <div v-for="(serie, index) in store.series" :key="index">
+        <div class="front">
+          <img
+            :src="`https://image.tmdb.org/t/p/w500${serie.poster_path}`"
+            :alt="original_title"
+            :title="original_title"
+          />
+        </div>
+
         <h2 class="py-1">{{ serie.name }}</h2>
         <h3 class="py-1">Titolo Originale: {{ serie.original_title }}</h3>
         <span>Lingua:</span>
@@ -58,5 +73,14 @@ img {
   object-fit: contain;
   height: 20px;
   border-radius: 10px;
+}
+.front {
+  height: 100%;
+
+  img {
+    object-fit: contain;
+    height: 450px;
+    border-radius: 10px;
+  }
 }
 </style>
